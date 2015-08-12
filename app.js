b9b662de86992fc745d7f9d6d5f8bbd395d15165
ajcumine <ajcumine@gmail.com>
2015-08-12 12:15:37 +0100

    Favicon and header images

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
 
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.static('public'));
 
 app.set('views', './views')
