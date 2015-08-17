c0a5170f1aabb9b25d20653cbe39cdfe60c7716b
ajcumine <ajcumine@gmail.com>
2015-08-17 14:33:33 +0100

    routing and layout for useful links

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+app.get('/useful-links', function (req, res) {
+  res.render('useful-links');
+});
+
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
